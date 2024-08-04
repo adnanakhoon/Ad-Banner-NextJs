8585972776b96739953d172a7f16d4a4cca38c00
adnanakhoon <adnanakhoon08@gmail.com>
2024-08-04 14:34:27 +0530

    changes in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-   
-  };
-
-export default nextConfig;
-
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   assetPrefix: './',
